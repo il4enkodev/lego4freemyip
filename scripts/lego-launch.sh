1f3c518a375417baff15d98e6f8c55e0d96d4078
Pavel Ilchenko <il4enkodev@gmail.com>
2023-03-09 11:09:46 +0200

    Use /etc/lego as LEGO_PATH directory

--- scripts/lego-launch.sh
+++ scripts/lego-launch.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export LEGO_PATH=/etc/lego
+
 obtain() {
 	lego -a --email=$LEGO_EMAIL --domains="*.$FREEMYIP_DOMAIN" --dns freemyip -k ec384 \
         run --must-staple --run-hook=$HOOK
